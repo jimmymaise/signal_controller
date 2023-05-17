162bf3ca4fb00c300d8ac05bb6681cc26eebd6c1
jimmymaise <the@duyet.com>
2023-05-17 10:57:23 -0700

    complete fix bug

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,22 +1,112 @@
 from django.contrib import admin
-from .models import MasterTrader, Signal,CrawlerType,CrawlAssignment,CrawlRunner
+from .models import MasterTrader, Signal, CrawlerType, CrawlAssignment, CrawlRunner
+
+
+def _save_model(request, obj, form, change):
+    update_fields = set()
+    if change:
+        for key, value in form.cleaned_data.items():
+            # assuming that you have ManyToMany fields that are called groups and user_permissions
+            # we want to avoid adding them to update_fields
+            if key in ["user_permissions", "groups"]:
+                continue
+            if value != form.initial[key]:
+                update_fields.add(key)
+
+    obj.save(update_fields=update_fields)
 
 
 class MasterTraderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'external_trader_id', 'source', 'is_active')
+    list_display = (
+        "id",
+        "external_trader_id",
+        "source",
+        "is_active",
+        "updated_at",
+        "created_at",
+    )
+
+    def save_model(self, request, obj, form, change):
+        """
+        Given a model instance save it to the database.
+        """
+        if change:
+            _save_model(request, obj, form, change)
+        else:
+            super().save_model(request, obj, form, change)
 
 
 class SignalAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'external_signal_id', 'symbol', 'type', 'size',
-        'price_order', 'stop_loss', 'take_profit',
-        'market_price', 'trader', 'time')
+        "id",
+        "external_signal_id",
+        "symbol",
+        "type",
+        "size",
+        "price_order",
+        "stop_loss",
+        "take_profit",
+        "market_price",
+        "trader",
+        "time",
+        "updated_at",
+        "created_at",
+    )
+
+
+class CrawlRunnerAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "crawler_type",
+        "is_active",
+        "updated_at",
+        "created_at",
+    )
+
+    def save_model(self, request, obj, form, change):
+        """
+        Given a model instance save it to the database.
+        """
+        if change:
+            _save_model(request, obj, form, change)
+        else:
+            super().save_model(request, obj, form, change)
+
+
+class CrawlerTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "source",
+        "is_active",
+        "updated_at",
+        "created_at",
+    )
+
+    def save_model(self, request, obj, form, change):
+        """
+        Given a model instance save it to the database.
+        """
+        _save_model(request, obj, form, change)
+
+
+class CrawlAssignmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "master_trader",
+        "crawl_runner",
+        "updated_at",
+        "created_at",
+    )
+
+    def save_model(self, request, obj, form, change):
+        """
+        Given a model instance save it to the database.
+        """
+        _save_model(request, obj, form, change)
 
 
 # Register your models here.
 admin.site.register(MasterTrader, MasterTraderAdmin)
 admin.site.register(Signal, SignalAdmin)
-admin.site.register(CrawlerType)
-admin.site.register(CrawlAssignment)
-admin.site.register(CrawlRunner)
-
+admin.site.register(CrawlerType, CrawlerTypeAdmin)
+admin.site.register(CrawlAssignment, CrawlAssignmentAdmin)
+admin.site.register(CrawlRunner, CrawlRunnerAdmin)
--- src/api/models.py
+++ src/api/models.py
@@ -13,12 +13,14 @@ class MasterTrader(models.Model):
     external_trader_id = models.CharField(max_length=60)
     source = models.CharField(max_length=255, choices=SOURCE_CHOICES)
     is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.id}({self.external_trader_id}|{self.source})"
+        return f"{self.source}|{self.external_trader_id}"
 
     def update_master_trader_signals(self, updated_signal_list):
-        signal_list_from_db = Signal.objects.filter(trader_id=self.id).values(
+        signal_list_from_db = Signal.objects.filter(trader__pk=self.id).values(
             "id", "external_signal_id"
         )
         signal_dict_from_db = {
@@ -30,27 +32,22 @@ class MasterTrader(models.Model):
         to_update = []
         for updated_signal in updated_signal_list:
             updated_signal["trader_id"] = self.id
-            external_signal_id = updated_signal.get("external_signal_id")
+            external_signal_id = str(updated_signal.get("external_signal_id"))
 
             if exist_signal := signal_dict_from_db.get(external_signal_id):
-                # Update existing employee
                 to_update.append(Signal(id=exist_signal["id"], **updated_signal))
                 del signal_dict_from_db[external_signal_id]
             else:
-                # Create new employee
                 to_create.append(Signal(**updated_signal))
 
-        # Mark remaining employees for deletion
         to_delete_ids = [
             signal["id"] for external_signal_id, signal in signal_dict_from_db.items()
         ]
 
         # Perform database updates within a transaction for consistency
         with transaction.atomic():
-            # Delete employees marked for deletion
             Signal.objects.filter(id__in=to_delete_ids).delete()
 
-            # Update existing employees
             if to_update:
                 Signal.objects.bulk_update(
                     to_update,
@@ -63,13 +60,15 @@ class MasterTrader(models.Model):
                         "stop_loss",
                         "take_profit",
                         "market_price",
-                        "limit",
                     ],
                 )
 
-            # Create new employees
-
-            Signal.objects.bulk_create(to_create)
+            if to_create:
+                print(
+                    "Signal will be created",
+                    [(item.trader_id, item.external_signal_id) for item in to_create],
+                )
+                Signal.objects.bulk_create(to_create)
 
     class Meta:
         unique_together = ("external_trader_id", "source")
@@ -88,10 +87,11 @@ class Signal(models.Model):
     stop_loss = models.FloatField()
     take_profit = models.FloatField()
     market_price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.external_signal_id} (from {self.trader.external_trader_id}-{self.trader.source}|updated at {self.time})"
-
 
     class Meta:
         unique_together = ("trader", "external_signal_id")
@@ -101,12 +101,22 @@ class CrawlerType(models.Model):
     name = models.CharField(max_length=255, unique=True, primary_key=True)
     source = models.CharField(max_length=255, choices=SOURCE_CHOICES)
     is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class CrawlRunner(models.Model):
     name = models.CharField(max_length=255, unique=True)
     crawler_type = models.ForeignKey(CrawlerType, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} (type: {self.crawler_type})"
 
 
 class CrawlAssignment(models.Model):
@@ -114,6 +124,11 @@ class CrawlAssignment(models.Model):
     crawl_runner = models.ForeignKey(
         CrawlRunner, on_delete=models.CASCADE, related_name="assignments"
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Runner:{self.crawl_runner}|Trader:{self.crawl_runner}"
 
 
 @receiver(post_save, sender=MasterTrader)
@@ -121,14 +136,17 @@ class CrawlAssignment(models.Model):
 @receiver(post_delete, sender=MasterTrader)
 @receiver(post_delete, sender=CrawlRunner)
 def balance_runner_assignment_on_change(sender, **kwargs):
-    balance_runner_assignment()
+    if "created" not in kwargs or kwargs["created"] == True or kwargs["update_fields"]:
+        balance_runner_assignment()
 
 
 def assign_more_master_traders_to_a_crawl_runner(
     crawl_runner, num_additional_master_trader
 ):
     unassigned_master_traders = MasterTrader.objects.filter(
-        is_active=True, crawlassignment__isnull=True, source=crawl_runner.crawler_type.source
+        is_active=True,
+        crawlassignment__isnull=True,
+        source=crawl_runner.crawler_type.source,
     )
 
     CrawlAssignment.objects.bulk_create(
@@ -148,14 +166,14 @@ def balance_runner_assignment():
         | Q(master_trader__isnull=True)
         | Q(crawl_runner__is_active=False)
         | Q(crawl_runner__isnull=True)
-    ).update(crawl_runner=None)
+    ).delete()
 
     # Get all active crawl_runners
     active_crawl_runners = (
         CrawlRunner.objects.filter(is_active=True)
         .annotate(num_assignments=Count("assignments"))
         .prefetch_related("assignments")
-        .order_by("num_assignments")
+        .order_by("-num_assignments")
     )
 
     # Get the total number of active crawl_runners and active master_traders
@@ -163,6 +181,9 @@ def balance_runner_assignment():
         "id", flat=True
     ).count()
     total_active_master_traders = MasterTrader.objects.filter(is_active=True).count()
+
+    if not all([total_active_master_traders, total_active_crawl_runners]):
+        return
 
     # Calculate the target number of master_traders per active crawl_runner
     avg_master_traders_per_crawl_runner = (
@@ -186,7 +207,9 @@ def balance_runner_assignment():
             assignment_to_delete = crawl_runner.assignments.all()[
                 : abs(additional_master_traders)
             ]
-            assignment_ids_to_delete = [assignment.id for assignment in assignment_to_delete]
+            assignment_ids_to_delete = [
+                assignment.id for assignment in assignment_to_delete
+            ]
             CrawlAssignment.objects.filter(id__in=assignment_ids_to_delete).delete()
 
         # Assign additional master_traders to this crawl_runner
