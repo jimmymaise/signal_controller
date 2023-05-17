f15949e4818ae98fcc9fe06bf20fdca87fb73c43
jimmymaise <the@duyet.com>
2023-05-17 10:57:23 -0700

    update db setting

--- src/app/settings.py
+++ src/app/settings.py
@@ -114,7 +114,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 DB_PASSWORD = os.getenv("DB_PASSWORD", "myproject_password181")
 DB_NAME = os.getenv("DB_NAME", "myproject_db")
-DB_USER = os.getenv("DB_USER", "myproject_user")
+DB_USER = os.getenv("DB_USER", "postgres")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", 5432)
 
